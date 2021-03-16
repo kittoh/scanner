76e9b0341a7fe280b4ef46baff5e503c8aa51855
Kit Hernandez <kitto.hernandes@gmail.com>
2021-03-16 14:05:04 +0800

    TRY

--- open_jira_issues.py
+++ open_jira_issues.py
@@ -3,9 +3,72 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 import getpass
+import click
 
 ISSUE_SUMMARY = "SAMPLE SUMMARY"
 ISSUE_DESCRIPTION = "SAMPLE DESCRIPTION"
+
+@click.command(
+    short_help = 'This is a short help.'
+)
+
+@click.option(
+    '--project',
+    required=True,
+    help="The 3-4 character JIRA Project key."
+)
+
+@click.option(
+    '--auth-file',
+    required=True,
+    type=click.Path(exists=True),
+    help='Path to the account authorization details JSON file.'
+)
+
+@click.option(
+    '--report-file',
+    required=True,
+    type=click.Path(exists=True),
+    help='Path to the HTML Report file.'
+)
+
+@click.option(
+    '--server',
+    # default="https://jira.atlassian.com",
+    required=True,
+    type=str,
+    help='The JIRA server.'
+)
+
+def jira_login(server):
+    email = "kitto.hernandes@gmail.com"        # input("JIRA email: ")
+    password = getpass.getpass(prompt = "JIRA password: ")
+    # api_key = input("API key: ")
+    options = {
+    "server": server
+    }
+
+    auth_jira = JIRA(
+        options=options,
+        basic_auth=(
+            username,
+            api_key
+        )
+    )
+    return auth_jira
+
+    auth_jira = HTTPBasicAuth(
+        email,
+        api_key
+    )
+  
+    return auth_jira
+
+
+
+
+
+
 
 # your jira domain
 server = "https://kittoh.atlassian.net/rest/api/3/issue"
@@ -22,23 +85,26 @@ api_key = input("API key: ")
 # auth = HTTPBasicAuth("kitto.hernandes@gmail.com", api_key)
 
 # open parsed report
+def add_attachment(jira, issue, attachment_path):
+    """
+    Adds an attachment.
+    :param jira: JIRA session
+    :param issue: Issue object
+    :param attachment_path: Path to the file you want to attach
+    :return:
+    """
+    # upload file from `/some/path/attachment.txt`
+    jira.add_attachment(issue=issue, attachment=attachment_path)
+
+    # read and upload a file (note binary mode for opening, it's important):
+    with open(attachment_path, 'rb') as f:
+        jira.add_attachment(issue=issue, attachment=f)
+    print(f"Uploaded: {attachment_path}")
+
 with open('test_report_parsed.json') as j:
     report = json.load(j)
 
-def jira_login(server):
-    email = "kitto.hernandes@gmail.com"        # input("JIRA email: ")
-    password = getpass.getpass(prompt="JIRA password:")
-    api_key = input("API key: ")
-    options = {
-    "server": server
-    }
 
-    auth_jira = HTTPBasicAuth(
-        email,
-        api_key
-    )
-  
-    return auth_jira
 
 # jira_login(server)
 
---
+++ try.py
@@ -0,0 +1,123 @@
+from jira import JIRA
+import click
+import getpass
+
+ISSUE_SUMMARY = "TEST SUMMARY"
+ISSUE_DESCRIPTION = """TEST DESCRIPTION
+1.
+2.
+3.
+"""
+
+
+@click.command(
+    short_help='Open a JIRA ticket with your Cloudsplaining report findings.'
+)
+# By default, the client will connect to a JIRA instance started from the Atlassian Plugin SDK
+# (see https://developer.atlassian.com/display/DOCS/Installing+the+Atlassian+Plugin+SDK for details).
+@click.option(
+    '--project',
+    required=True,
+    help="The 3-4 character JIRA Project key."
+)
+# @click.option(
+#     '--auth-file',
+#     required=True,
+#     type=click.Path(exists=True),
+#     help='Path to the account authorization details JSON file.'
+# )
+# @click.option(
+#     '--report-file',
+#     required=True,
+#     type=click.Path(exists=True),
+#     help='Path to the HTML Report file.'
+# )
+# @click.option(
+#     '--triage-file',
+#     required=True,
+#     help='Path to the Cloudsplaining Triage worksheet.'
+# )
+# @click.option(
+#     '--data-file',
+#     required=True,
+#     type=click.Path(exists=True),
+#     help='Path to the JSON Data file.'
+# )
+@click.option(
+    '--server',
+    # default="https://jira.atlassian.com",
+    required=True,
+    type=str,
+    help='The JIRA server.'
+)
+def open_jira_ticket(project, server):
+# def open_jira_ticket(project, auth_file, report_file, triage_file, data_file, server):
+    jira = jira_login(server)
+    issue = jira.create_issue(
+        project=project,
+        summary=ISSUE_SUMMARY,
+        description=ISSUE_DESCRIPTION,
+        issuetype={
+            'name': 'Bug'
+        }
+    )
+    # for file in [auth_file, report_file, triage_file, data_file]:
+    add_attachment(jira, issue, attachment_path)
+
+    print("Issue opened and attachments added. Metadata:")
+    print(f"\tIssue ID: {issue.id}")
+    print(f"\tIssue Key: {issue.key}")
+    print("Attachments")
+    list_attachments(issue)
+
+
+def jira_login(server):
+    # username = input("Enter your JIRA username")
+    # password = getpass.getpass(prompt='Enter your JIRA password.')
+    import os
+    username = os.getenv("JIRA_EMAIL")
+    api_key = os.getenv("JIRA_API_KEY")
+    options = {
+        "server": server,
+    }
+    # Supporting HTTP BASIC Auth right now.
+    # You can extend this script to support Cookie-based, OAuth or Kerberos."""
+    # Docs: https://jira.readthedocs.io/en/master/examples.html#authentication
+    auth_jira = JIRA(
+        options=options,
+        basic_auth=(
+            username,
+            api_key
+        )
+    )
+    return auth_jira
+
+
+def add_attachment(jira, issue, attachment_path):
+    """
+    Adds an attachment.
+    :param jira: JIRA session
+    :param issue: Issue object
+    :param attachment_path: Path to the file you want to attach
+    :return:
+    """
+    # upload file from `/some/path/attachment.txt`
+    # attachment_path = 'S:\K\Haxx\codes\test_report_parsed.json'
+    jira.add_attachment(issue=issue, attachment=attachment_path)
+
+    # read and upload a file (note binary mode for opening, it's important):
+    with open('test_report_parsed.json', 'rb') as f:
+        jira.add_attachment(issue=issue, attachment_path=f)
+    print(f"Uploaded: {attachment_path}")
+
+
+def list_attachments(issue):
+    for attachment in issue.fields.attachment:
+        print("Name: '{filename}', size: {size}".format(
+            filename=attachment.filename, size=attachment.size))
+        # to read content use `get` method:
+        print("Content: '{}'".format(attachment.get()))
+
+
+if __name__ == '__main__':
+    open_jira_ticket()
