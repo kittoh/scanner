4b57e01b517901930bec397d9464606e3ed30479
kittoh <kitto.hernandes@gmail.com>
2021-03-27 17:16:21 +0800

    update

--- run_zap2.sh
+++ run_zap2.sh
@@ -5,11 +5,11 @@ docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py \
         -t http://testhtml5.vulnweb.com -g gen.conf -J test_report.json
 sleep 1
 
-# FILE = "test_report.json"
+# # FILE = "test_report.json"
 
-if [ -e "test_report.json" ]
-then
-    python JSON_parser.py
-fi
+# if [ -e "test_report.json" ]
+# then
+#     python JSON_parser.py
+# fi
 
 
